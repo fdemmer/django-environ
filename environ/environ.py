31eda5641b88cf23baccdffb08e8d4163c3fcb4e
Florian Demmer <fdemmer@gmail.com>
2017-06-29 22:28:22 +0200

    Bump version: 0.4.4-fdemmer.1 → 0.4.4-fdemmer.2

--- environ/environ.py
+++ environ/environ.py
@@ -23,7 +23,7 @@ from six import string_types
 logger = logging.getLogger(__name__)
 
 
-VERSION = '0.4.4-fdemmer.1'
+VERSION = '0.4.4-fdemmer.2'
 __author__ = 'joke2k'
 __version__ = tuple(VERSION.split('.'))
 
